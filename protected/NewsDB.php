35f9ae07ccbe01aff5f9d3b48ec9c0e7cc35a912
ivankip <1209qwpo3487>
2016-05-12 21:58:46 +0500

    Создана "база данных" новостей

---
+++ protected/NewsDB.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    '1' => [
+        'title' => 'Первая новоесть',
+        'text'  => 'Текст первой новости' ,
+        'image' => 'http://www.wpclipart.com/education/gold_stars/more_stars/star_number_1_T.png'
+    ],
+
+    '2' => [
+        'title' => 'Вторая новость',
+        'text'  => 'Текст второй новости',
+        'image' => 'http://www.theartoftakingaction.com/wp-content/uploads/2012/05/gold-number-2.jpg'
+    ],
+
+    '3' => [
+        'title' => 'Третья новость',
+        'text'  => 'Текст третьей новости',
+        'image' => 'http://images.clipshrine.com/getimg/PngMedium-Number-3-3876.png'
+    ],
+
+];
