e0ead1d005dc1cdb5d9cbeb6d6bd649adaabe79e
ivankip <1209qwpo3487>
2016-05-16 23:28:16 +0500

    Создана модель на основе миграции

--- protected/Models/Article.php
+++ protected/Models/Article.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
-use T4\Core\Std;
+use T4\Orm\Model;
 
+/**
+ * Class Article
+ * @package App\Models
+ * Класс Article
+ * Схема таблицы
+ */
 class Article
-    extends Std
+    extends Model
 {
+
+    static protected $schema = [
+        'columns' => [
+            '__id'      => ['type'  => 'pk'],
+            'title'     => ['type'  => 'string'],
+            'text'      => ['type'  => 'text'],
+            'dateTime'  => ['type'  => 'datetime']
+        ]
+    ];
 
 }
