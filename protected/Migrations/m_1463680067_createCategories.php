5688fbb68f7f129ab15beff375afcef2186b1260
ivankip <1209qwpo3487>
2016-05-19 23:41:52 +0500

    Миграция и модель Категорий

---
+++ protected/Migrations/m_1463680067_createCategories.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Migrations;
+
+use T4\Orm\Migration;
+
+class m_1463680067_createCategories
+    extends Migration
+{
+
+    public function up()
+    {
+        $this->createTable('categories', [
+            'columns' => [
+                'title' => ['type' => 'string']
+            ]
+        ], [], ['tree']);
+    }
+
+    public function down()
+    {
+        $this->dropTable('categories');
+    }
+    
+}
---
+++ protected/Models/Category.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+/**
+ * Class Category
+ * @package App\Models
+ * @property string $title
+ */
+class Category
+{
+    static protected $schema =[
+        'table' => 'categories',
+        'columns' => [
+            'title' => ['type' => 'string']
+        ]
+    ];
+
+    static protected $extensions = ['tree'];
+
+}
